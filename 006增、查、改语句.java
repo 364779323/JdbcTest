bb82f8b3118e6849149d5fb631d22fa2c58d79a7
Leo <39918089+364779323@users.noreply.github.com>
2018-12-04 12:55:13 +0800

    Create 006增、查、改语句.java

---
+++ 006增、查、改语句.java
@@ -0,0 +1,3 @@
+ String create="insert into hero values(null,"+"'timo'"+","+ 313.0f +","+ 55 +")";
+ String delete="delete from hero while id = 5";
+ String update="update hero set name = 'name 5' where id=3";
